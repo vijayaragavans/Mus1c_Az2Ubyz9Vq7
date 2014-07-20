5aafe89429b760682c6bddd3af1582f59848ba94
vijayaragavan <msjvmv@gmail.com>
2014-07-20 15:27:09 +0530

    modified for Filenames

--- protected/controllers/TblsongsController.php
+++ protected/controllers/TblsongsController.php
@@ -164,7 +164,7 @@ class TblSongsController extends Controller
 			'model'=>$model,
 		));
 	}
-
+ 
 	/**
 	 * Returns the data model based on the primary key given in the GET variable.
 	 * If the data model is not found, an HTTP exception will be raised.

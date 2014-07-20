ed17021abbb81ffefe9790c7ee66e48f83e6360c
vijayaragavans <msjvmv@gmail.com>
2014-07-20 15:53:31 +0530

    modified for _view

--- protected/views/tblalbumcategory/_view.php
+++ protected/views/tblalbumcategory/_view.php
@@ -6,8 +6,7 @@
               <tr>
 	<td><?php echo CHtml::link(CHtml::encode($data->album_category_id), array('view', 'id'=>$data->album_category_id)); ?></td>
 	<td><?php echo CHtml::encode($data->album_category_name); ?></td>
-	<td><?php echo CHtml::encode($data->album_category_created_by); ?></td>
 	<td><?php echo CHtml::encode($data->album_category_is_active); ?></td>
-	<td><?php echo CHtml::encode($data->album_category_created_on); ?></td>
-	<td>Edit</td>
+	<td><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/tblalbumcategory/update/<?php echo $data->album_category_id; ?>">Edit</a></td>
+	<td><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/tblalbumcategory/delete/<?php echo $data->album_category_id; ?>">Delete</a></td>
 	</tr>

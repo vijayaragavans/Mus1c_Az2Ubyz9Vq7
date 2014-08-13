5573097a072512c2f73f015216035f0451c58803
vijayaragavans <msjvmv@gmail.com>
2014-08-13 22:57:17 +0530

    modified

--- protected/controllers/AlbumpagecategoriesController.php
+++ protected/controllers/AlbumpagecategoriesController.php
@@ -1,6 +1,6 @@
 <?php
 
-class AlbumPageCategoriesController extends Controller
+class AlbumpagecategoriesController extends Controller
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
